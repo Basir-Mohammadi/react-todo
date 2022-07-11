8cc2a97d4abf6d64ff32f549807a171bdca090cc
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-11 14:55:45 +0100

    Updating the state using the setState() method

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -23,7 +23,14 @@ class TodoContainer extends React.Component {
    };
 
    handleChange = (id) => {
-    console.log("clicked",id);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id){
+           todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    })
    };
    render() {
     return (
