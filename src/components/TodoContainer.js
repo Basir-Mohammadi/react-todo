29384cd033de0826279b832559a90d2473249e78
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-11 16:13:59 +0100

    Adding a text input field and a submit button

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import TodosList from "./TodosList";
 import Header from "./Header"
+import InputTodo from "./InputTodo";
 class TodoContainer extends React.Component {
   state = {
     todos: [
@@ -51,7 +52,8 @@ class TodoContainer extends React.Component {
    render() {
     return (
       <div>
-         <Header />
+        <Header />
+        <InputTodo />
         <TodosList todos={this.state.todos}
         handleChangeProps = {this.handleChange} 
         deleteTodoProps ={this.delTodo}/>
