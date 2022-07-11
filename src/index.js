8e8cd6de8b1f60a0ca0abffb228447d6b81c939f
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-11 12:30:58 +0100

    Create react setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./components/TodoContainer"
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
