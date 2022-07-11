638f3447e65cd4fc56e25e0df3c35a6ba4ad9ca7
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-11 16:20:32 +0100

    Handling React form that has more than one text input field

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -7,10 +7,10 @@ class InputTodo extends Component {
 
     onChange = (e) => {
         this.setState({
-            title: e.target.value
+            [e.target.name]: e.target.value
         })
     }
-    
+
   render() {
     return (
       <form>
@@ -18,6 +18,7 @@ class InputTodo extends Component {
         type="text" 
         placeholder="Add Todo..."
         value={this.state.title}
+        name="title"
         onChange ={this.onChange}
         />
         <button>Submit</button>
