d14126e4f996e35db9a6ff5c222cf7a44a58253a
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-11 16:31:47 +0100

    Generating random ids for the todos list items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,21 +2,22 @@ import React from "react"
 import TodosList from "./TodosList";
 import Header from "./Header"
 import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4,
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4,
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4,
         title: "Deploy to live server",
         completed: false
       }
@@ -51,7 +52,7 @@ class TodoContainer extends React.Component {
 
   addTodoItem = title => {
     const newTodo = {
-      id: 4,
+      id: uuidv4,
       title: title,
       completed: false
     };
