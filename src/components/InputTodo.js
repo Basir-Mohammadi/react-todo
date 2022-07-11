a0aca483b28b737a6799255d9383523f0103a79b
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-11 16:36:25 +0100

    Validating the Form Submission

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -13,10 +13,14 @@ class InputTodo extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
+        if(this.state.title.trim()){
         this.props.addTodoProps(this.state.title);
         this.setState({
             title: ""
           });
+        } else {
+            alert("please write item")
+        }
       };
 
   render() {
