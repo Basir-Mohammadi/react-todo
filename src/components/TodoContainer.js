498c541a34c373b6ccca99292577af83fb07cbb0
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-13 18:32:16 +0100

    Fetching data from a remote endpoint

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,23 +5,7 @@ import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
+    todos: []
    };
 
    handleChange = (id) => {
@@ -70,6 +54,12 @@ class TodoContainer extends React.Component {
         return todo
       }),
     })
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }));
   }
 
    render() {
