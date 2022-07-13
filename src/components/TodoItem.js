ab0fffc289e4434bd786eace0e6a4318285754b8
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-13 16:03:56 +0100

    Detecting when the Users press the Enter key to submit edited Items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -12,6 +12,12 @@ class TodoItem extends React.Component {
   })
   }
 
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
   render() {
     let viewMode = {}
     let editMode = {}
@@ -51,6 +57,7 @@ class TodoItem extends React.Component {
      onChange={e => {
       this.props.setUpdate(e.target.value, id)
     }}
+    onKeyDown={this.handleUpdatedDone}
      />
   </li>
   )
